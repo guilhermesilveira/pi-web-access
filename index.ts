marcfargas/pi-subagents

fix: adapt execute signature to pi v0.51.0
The v0.51.0 release changed ToolDefinition.execute parameter order
from (toolCallId, params, onUpdate, ctx, signal) to
(toolCallId, params, signal, onUpdate, ctx).

index.ts
@@ -162,7 +162,7 @@ CHAIN DATA FLOW:
 Example: { chain: [{agent:"scout", task:"Analyze {task}"}, {agent:"planner", task:"Plan based on {previous}"}] }`,
 		parameters: SubagentParams,
 
-		async execute(_id, params, onUpdate, ctx, signal) {
+		async execute(_id, params, signal, onUpdate, ctx) {
 			const scope: AgentScope = params.agentScope ?? "user";
 			baseCwd = ctx.cwd;
 			currentSessionId = ctx.sessionManager.getSessionFile() ?? `session-${Date.now()}-${Math.random().toString(36).slice(2, 8)}`;
@@ -632,7 +632,7 @@ Example: { chain: [{agent:"scout", task:"Analyze {task}"}, {agent:"planner", tas
 		description: "Inspect async subagent run status and artifacts",
 		parameters: StatusParams,
 
-		async execute(_id, params) {
+		async execute(_id, params, _signal, _onUpdate, _ctx) {
 			let asyncDir: string | null = null;
 			let resolvedId = params.id;
 

guilhermesilveira/pi-web-access

index.ts
@@ -172,7 +172,7 @@ export default function (pi: ExtensionAPI) {
 			domainFilter: Type.Optional(Type.Array(Type.String(), { description: "Limit to domains (prefix with - to exclude)" })),
 		}),
 
-		async execute(_toolCallId, params, onUpdate, _ctx, signal) {
+		async execute(_toolCallId, params, signal, onUpdate, _ctx) {
 			const queryList = params.queries ?? (params.query ? [params.query] : []);
 			if (queryList.length === 0) {
 				return {
@@ -395,7 +395,7 @@ export default function (pi: ExtensionAPI) {
 			urls: Type.Optional(Type.Array(Type.String(), { description: "Multiple URLs (parallel)" })),
 		}),
 
-		async execute(_toolCallId, params, onUpdate, _ctx, signal) {
+		async execute(_toolCallId, params, signal, onUpdate, _ctx) {
 			const urlList = params.urls ?? (params.url ? [params.url] : []);
 			if (urlList.length === 0) {
 				return {
@@ -548,7 +548,7 @@ export default function (pi: ExtensionAPI) {
 			urlIndex: Type.Optional(Type.Number({ description: "Get content for URL at index" })),
 		}),
 
-		async execute(_toolCallId, params, _onUpdate, _ctx, _signal) {
+		async execute(_toolCallId, params, _signal, _onUpdate, _ctx) {
 			const data = getResult(params.responseId);
 			if (!data) {
 				return {